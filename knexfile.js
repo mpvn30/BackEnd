65d1b7da6885d66942ca2ffb277742f4f4dddf48
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-11-17 21:08:54 -0500

    login and register added

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,30 @@
+// Update with your config settings.
+
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    connection: {
+      filename: './data/sleeptracker.db3',
+    },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: './data/migrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/test.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
   },
 };
